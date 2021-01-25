6d25fb78173ff84620c67c3098b30dc39ae2455e
SgtClubby <assassin12@hotmail.no>
2021-01-25 14:54:49 +0100

    removed comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ const app = require('./app');
 
 const port = 4242;
 app.listen(port, () => {
-  /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
 });
